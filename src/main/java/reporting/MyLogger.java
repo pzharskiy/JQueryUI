56ba12576e8ff09f6b52a20c87f5845943fa7705
Pavel Zharski <pavel_zharski@epam.com>
2019-04-01 08:56:51 +0300

    logger

---
+++ src/main/java/reporting/MyLogger.java
@@ -0,0 +1,39 @@
+package reporting;
+
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
+public class MyLogger {
+
+    public static Logger logger = Logger.getLogger(MyLogger.class);
+
+    public static void error(String message){
+        logger.error(message);
+    }
+
+    public static void trace(String message){
+        logger.trace(message);
+    }
+
+    public static void error(String message, Throwable throwable){
+        logger.error(message, throwable);
+    }
+
+    public static void info(String message){
+        logger.info(message);
+    }
+
+    public static void debug(String message){
+        logger.debug(message);
+    }
+
+    public static void warn(String message){
+        logger.warn(message);
+    }
+
+    public static void log(Priority priority, Object message){
+        logger.log(priority, message);
+    }
+
+}
